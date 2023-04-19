598f606abbf25c9c0c7acb865fbe38362c853386
henningcon <94714295+henningcon@users.noreply.github.com>
2023-04-19 13:18:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ if "Flow" in measurements:
 if len(axes) > 0:
     chart = alt.layer(*(axes)).resolve_scale(
         y='independent'
-    ).interactive(bind_y=False).properties(height=520)
+    ).interactive(bind_y=False).properties(height=480)
 
     # display the chart
     st.altair_chart(chart, use_container_width=True)
