59629d8a1cdc1364fd606feed084b928996d55c4
henningcon <94714295+henningcon@users.noreply.github.com>
2023-05-04 02:17:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ st.markdown(css, unsafe_allow_html=True)
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 0rem;
                     padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
+                    padding-left: 0rem;
+                    padding-right: 0rem;
                 }
         </style>
         """, unsafe_allow_html=True)
